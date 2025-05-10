23b76df2f72f9ec6420e5871b28d4aa49ed82caa
maite <terealbenca@gmail.com>
2025-05-10 12:33:39 +0200

    add new experiences and skills

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -1,29 +1,46 @@
-"use client";
-import React, { useTransition, useState } from "react";
-import Image from "next/image";
-import TabButton from "./TabButton";
+"use client"
+import React, { useTransition, useState } from "react"
+import TabButton from "./TabButton"
 
 const TAB_DATA = [
   {
     title: "Skills",
     id: "skills",
     content: (
-      <ul className="list-disc pl-2">
-        <li>Node.js</li>
-        <li>Express</li>
-        <li>Mongo DB</li>
-        <li>MySQL</li>
-        <li>PHP</li>
-        <li>JavaScript</li>
-        <li>React</li>
-        <li>Css</li>
-        <li>Tailwind</li>
-        <li>Bootstrap</li>
-        <li>Surge</li>
-        <li>Render</li>
-        <li>Netlifly</li>
-        <li>Vercel</li>
-      </ul>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <ul className="list-disc pl-4">
+          <li>Node.js</li>
+          <li>Express</li>
+          <li>Mongo DB</li>
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>Typescript</li>
+          <li>Firebase</li>
+          <li>Multer</li>
+          <li>Tailwind</li>
+          <li>Curl</li>
+          <li>Surge</li>
+          <li>Render</li>
+          <li>Netlify</li>
+          
+        </ul>
+        <ul className="list-disc pl-4">
+          <li>PHP</li>
+          <li>Java</li>
+          <li>Springboot</li>
+          <li>Maven</li>
+          <li>Tomcat</li>
+          <li>PHP</li>
+          <li>MySQL</li>
+          <li>CSS</li>
+          <li>Bootstrap</li>
+          <li>Mockito</li>
+          <li>Postman</li>
+          <li>Swagger</li> 
+          <li>Vercel</li>
+
+        </ul>
+      </div>
     ),
   },
   {
@@ -31,7 +48,10 @@ const TAB_DATA = [
     id: "education",
     content: (
       <ul className="list-disc pl-2">
-        <li>2024 UX Design: UX/UI user experience + Figma 2024 - Udemy </li>
+        <li>2025 Tailwind/bootstrap Certification - Hack a Boss Academy ( in progress)</li>
+        <li>2025 Typescript Certification - Hack a Boss Academy ( in progress)</li>
+        <li>2025 Bootcamp Java and Srpingboot - Hack a Boss Academy</li>
+        <li>2024 UX Design: UX/UI user experience + Figma 2024 - Udemy (in porgress)</li>
         <li>2024 Fullstack Bootcamp - MERN - Eurofirms</li>
         <li>2023 Development Certification Javascript algorithms and data structures - FreeCodeCamp</li>
         <li>2022-2023 Higher course of web development technician - CEAC</li>
@@ -46,7 +66,7 @@ const TAB_DATA = [
     id: "certifications",
     content: (
       <ul className="list-disc pl-2">
-        <li>2024 Certificate of Bootcamp Full Stack Developer - Eurofirms</li>
+        <li>2024 Full Stack Developer - Personal project of Art's academy (until now)</li>
         <li>2024 Developer business internships - Informatica Valencia</li>
         <li>2023 Designer business intership - Spanish Ole, Sevilla</li>
         <li>2008 II Animation prize -La Marvarrosa animation contest</li>
@@ -69,7 +89,7 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-12 xl:px-8">
-        <Image src="/images/about-image.png" width={500} height={500} />
+        {/* <Image src="/images/about-image.png" width={500} height={500} /> */}
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-sm">
